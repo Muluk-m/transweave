c7aca62383d23945c287d04ae12d846eb6af6ec9
Nain <maqiqian0316@163.com>
2025-04-22 17:56:48 +0800

    feat: update env

--- packages/web/next.config.mjs
+++ packages/web/next.config.mjs
@@ -1,11 +1,10 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'http://localhost:3001/api/:path*',
+        source: "/api/:path*",
+        destination: process.env.NEXT_INTERNAL_API_URL + "/api/:path*",
       },
     ];
   },
